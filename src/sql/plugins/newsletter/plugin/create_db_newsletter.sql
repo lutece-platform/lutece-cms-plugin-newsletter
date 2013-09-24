41933759b88f77ef6b322fb66b96b4691a0a0c4e
vbroussard <vbroussard@users.noreply.github.com>
2013-09-24 13:38:00 +0000

    NEWSLETTER-114 : Removed file 'create_newsletter_portlet.sql'. Its content is now in the file 'create_bd_newsletter.sql'
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-newsletter@51961 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/newsletter/plugin/create_db_newsletter.sql
+++ src/sql/plugins/newsletter/plugin/create_db_newsletter.sql
@@ -127,3 +127,22 @@ CREATE TABLE newsletter_topic_free_html (
   html_content LONG VARCHAR,
   PRIMARY KEY (id_topic)
 );
+
+--
+-- Creation of table newsletter_portlet_archive
+--
+CREATE TABLE newsletter_portlet_archive (  
+	id_portlet INT DEFAULT '0' NOT NULL,  
+	id_sending INT DEFAULT '0' NOT NULL,  
+	PRIMARY KEY (id_portlet,id_sending)
+);
+
+
+--
+-- Creation of table newsletter_portlet_subscribe
+--
+CREATE TABLE newsletter_portlet_subscribe (  
+	id_portlet INT DEFAULT '0' NOT NULL,  
+	id_newsletter INT DEFAULT '0' NOT NULL,  
+	PRIMARY KEY (id_portlet,id_newsletter)
+);
