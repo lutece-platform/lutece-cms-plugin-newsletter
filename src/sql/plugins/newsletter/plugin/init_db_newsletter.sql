a7aaba99ced57bb4964d173179bd24b07cae0227
HOHL <laurent.hohl@gmail.com>
2023-09-13 14:34:04 +0200

    LUT-26142 :  Reset newsletter images

--- src/sql/plugins/newsletter/plugin/init_db_newsletter.sql
+++ src/sql/plugins/newsletter/plugin/init_db_newsletter.sql
@@ -7,8 +7,7 @@ INSERT INTO newsletter_description values (1,'2007-05-16 13:03:30','Lettre d''in
 -- Dumping data for table newsletter_template
 --
 
-INSERT INTO newsletter_template VALUES (1,'newsletter_model','model_newsletter.html','modele_page1.gif','all','NEWSLETTER_TEMPLATE',2);
-INSERT INTO newsletter_template VALUES (2,'document_model','model_document.html','modele_document1.gif','all','NEWSLETTER_DOCUMENT',1);
+INSERT INTO newsletter_template VALUES (1,'newsletter_model','model_newsletter.html','model-newsletter.svg','all','NEWSLETTER_TEMPLATE',2);
 
 --
 -- Dumping data for table newsletter_properties
